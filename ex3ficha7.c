d6826f836f0ae2aed3db7fc2748760abeea426f8
pedroafonsoo <carlospmafonso@hotmail.com>
2015-12-26 14:42:29 +0000

    ex 3 da ficha 7

--- ex3ficha7.c
+++ ex3ficha7.c
@@ -1,16 +1,30 @@
 #include<stdio.h>
+#include<ctype.h>
 #include <string.h>
+#define TAM 100
 
 void main(){
 
+    int i;
+    char frase[TAM];
 
+    printf("Introduza uma frase \n");
+    gets(frase);
 
+    i=0;
+    while(frase[i]!='\0'){
+        //Salta por cima dos espaços no inicio da palavra
+        while((frase[i]==' ' || frase[i]=='\t') && frase[i] != '\0'){
+            i++;
+        }
+        //Escrever as letras da palavra
+        while(frase[i]!=' ' && frase[i]!='\t' && frase[i]!='\0'){
+            printf("%c",frase[i]);
+            i++;
+        }
 
-
-
-
-
-
-
+        if(frase[i] != '\0')
+            printf("\n");
+    }
 
 }
